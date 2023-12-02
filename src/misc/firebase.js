11f53a664d5f0fec84dadf01236d4aa17b413ab3
pradeep <pradeepbisen60@gmail.com>
2023-12-02 17:10:19 +0530

    initialization of project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBejY-aL680nkVGQf55mtoLTzNg-sSRRj4",
+    authDomain: "chatapp-22c4e.firebaseapp.com",
+    databaseURL: "https://chatapp-22c4e-default-rtdb.firebaseio.com",
+    projectId: "chatapp-22c4e",
+    storageBucket: "chatapp-22c4e.appspot.com",
+    messagingSenderId: "268422137513",
+    appId: "1:268422137513:web:a32906c3071c6a4c888bb8",
+    measurementId: "G-GYPW8FZ2MD"
+  };
+
+const chatapp = firebase.initializeApp(firebaseConfig)
