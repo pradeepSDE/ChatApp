91951c58f5b0c6b3d7114a0cbc00e7fbfd2e5873
pradeep <pradeepbisen60@gmail.com>
2024-02-15 17:08:58 +0530

    removed dark css

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -82,12 +82,12 @@ function SignUp() {
     <>
     <body className="bg-white rounded-lg ">  
 
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-gray-50 ">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
   <h1 className='text-2xl  font-bold tracking-wide text-transparent bg-clip-text bg-gradient-to-br from-sky-400 to-indigo-800'>P-Chat</h1>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                   Create New account
               </h1>
               {error &&  <div className='bg-red-200 p-2 rounded-2xl  '>
@@ -97,27 +97,27 @@ function SignUp() {
             }
               <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleSignup}>
                   <div>
-                      <label for="displayName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <label for="displayName" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
                       <input 
                       type="Username" 
                       name="Username" 
                       id="displayName" 
                       value={displayName}
                       onChange={(e)=>setdisplayName(e.target.value)} 
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required/>
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Username" required/>
                   </div>
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                       <input 
                       type="email" 
                       name="email" 
                       id="email" 
                       value={email}
                       onChange={(e)=>setEmail(e.target.value)} 
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required=""/>
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
                       <input 
                       type="password" 
                       name="password" 
@@ -125,21 +125,21 @@ function SignUp() {
                       placeholder="••••••••" 
                       value={password}
                       onChange={(e)=>setPassword(e.target.value)}
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
                           <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required=""/>
                           </div>
                           <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                            <label for="remember" class="text-gray-500 ">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline ">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <button type="submit" class="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign Up</button>
+                  <p class="text-sm font-light text-gray-500 ">
                   Already have an account?  <Link to={'/SignIn'} className="font-bold text-grey-700 underline" >  SignUp  </Link>
                   </p>
               </form>
