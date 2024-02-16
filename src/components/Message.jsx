94e3e9a9462106269ffe423c52280153e67a5452
pradeep <pradeepbisen60@gmail.com>
2024-02-16 14:14:49 +0530

    Revert "fixed msgNavbar"
    This reverts commit 5cea612f4befd1defde14892549869dc56eea9a1.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,49 +1,46 @@
-import React, { useRef, useState, useEffect } from "react";
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
-import ChatContext from "../context/ChatContext";
-import { Timestamp, arrayUnion, doc, updateDoc } from "firebase/firestore";
-import { db, messaging } from "../App";
-import { v4 as uuid } from "uuid";
-import { Messaging } from "firebase/messaging/sw";
-const Message = ({ messages }) => {
-  // console.log(messages)
-  const { currentUser } = useContext(AuthContext);
-  // console.log(currentUser)
-  const { data } = useContext(ChatContext);
+import React, { useRef, useState,useEffect } from 'react'
+import { useContext } from 'react'
+import AuthContext from '../context/AuthContext'
+import ChatContext from '../context/ChatContext'
+import { Timestamp, arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { db, messaging } from '../App'
+import { v4 as uuid } from 'uuid'
+import { Messaging } from 'firebase/messaging/sw';
+const Message = ({messages}) => {
+    // console.log(messages)
+    const {currentUser}=useContext(AuthContext)
+    // console.log(currentUser)
+    const {data}=useContext(ChatContext)
 
-  // console.log(messages.senderId)
-  // console.log(currentUser.uid)
+// console.log(messages.senderId)
+// console.log(currentUser.uid)
+   
 
-  const ref = useRef();
+const ref = useRef();
 
-  useEffect(() => {
-    ref.current?.scrollIntoView({ behavior: "smooth" });
-    console.log("mai e se");
-  }, [messages]);
+useEffect(() => {
+  ref.current?.scrollIntoView({ behavior: "smooth" });
+  console.log("mai e se")
+}, [messages]);
+
+
+
 
   return (
     <>
-      <div
-        ref={ref}
-        className={`message   object-scale-downn    overflow-y-auto p-3 flex items-center p-2.5 ${messages.senderId === currentUser.uid ? "justify-end " : "justify-start"} ${messages.senderId === currentUser.uid && "owner"}  `}
-      >
-        <div
-          className={`  w-12 h-12 rounded-full bg-rose-400 p-0.5  flex justify-start ${messages.senderId === currentUser.uid ? "hidden" : ""} `}
-        >
-          <img
-            src="https://th.bing.com/th/id/OIP.Gfp0lwE6h7139625a-r3aAHaHa?rs=1&pid=ImgDetMain   "
-            alt=""
-          />
-        </div>
-        <div
-          className={`messsageInfo rounded-3xl text-xl  p-2.5 pl-4 pr-4 m-2  ${messages.senderId === currentUser.uid ? " bg-blue-500 text-white" : " bg-slate-200"} p-1.5`}
-        >
-          <p>{messages.text}</p>
-        </div>
-      </div>
-    </>
-  );
-};
+  <div    ref={ref} className={`message   object-scale-downn   scroll-smooth overflow-y-auto p-3 flex items-center p-2.5 ${messages.senderId === currentUser.uid ? "justify-end " : "justify-start"} ${messages.senderId === currentUser.uid && "owner"}  `}>
+     
+    <div className= {`  w-12 h-12 rounded-full bg-rose-400 p-0.5  flex justify-start ${messages.senderId===currentUser.uid? "hidden":""} `}>
+    <img  src="https://th.bing.com/th/id/OIP.Gfp0lwE6h7139625a-r3aAHaHa?rs=1&pid=ImgDetMain   " alt="" />
+    </div>
+ <div className={`messsageInfo rounded-3xl text-xl  p-2.5 pl-4 pr-4 m-2  ${messages.senderId === currentUser.uid ? " bg-blue-500 text-white" : " bg-slate-200"} p-1.5`}>
+  <p>{messages.text}</p>
+ </div>
+     
+</div>
 
-export default Message;
+    </>
+  )
+}
+
+export default Message
