4c752e1e5b45f217304da1c2bbd4f055e1463d3d
pradeep <pradeepbisen60@gmail.com>
2024-02-17 14:16:50 +0530

    upload test

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,31 +1,35 @@
-import { updateProfile } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import { storage } from "../App";
 
 const Avatar = () => {
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser.displayName);
+  currentUser && console.log(currentUser.displayName);
   const date = new Date().getTime;
   const storageRef = ref(storage, `${currentUser.displayName + date}`);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("clg")
     const file = e.target[0].value;
+console.log(file)
 
-    await uploadBytesResumable(storageRef, file).then(() => {
-      getDownloadURL(storageRef).then(async (downloadURL) => {
-        try {
-          //Update profile
-          await updateProfile(currentUser.user, {
-            photoURL: downloadURL,
-          });
-        } catch (er) {
-          console.log(er);
-        }
-      });
-    });
+    const upload = await uploadBytesResumable(storageRef, file)
+    console.log(upload)
+  //   .then(() => {
+  //     getDownloadURL(storageRef).then(async (downloadURL) => {
+  //       try {
+  //         //Update profile
+  //         await updateProfile(currentUser.user, {
+  //           photoURL: downloadURL,
+  //         });
+  //       } catch (er) {
+  //         console.log(er);
+  //       }
+  //     });
+  //   });
     console.log("handle");
   };
   return (
