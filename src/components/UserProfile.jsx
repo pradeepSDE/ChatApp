3a0f5494f18b8f391539f347c0d8974268270993
pradeep <pradeepbisen60@gmail.com>
2024-02-20 12:43:45 +0530

    Revert "imputmsg correction"
    This reverts commit db091cff270288256cf1e90412327d2c063da51e.

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import SendMessage from "./SendMessage";
-import MessangerConsole from "./MessangerConsole";
+import React from 'react'
+import SendMessage from './SendMessage';
+import MessangerConsole from './MessangerConsole';
 
 function UserProfile() {
-  const user = JSON.parse(localStorage.getItem("user"));
+
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
-      <MessangerConsole />
+      {/* <h1>Hello!! {user && user.email}</h1> */}
+      
+      {/* <SendMessage/> */}
+      <MessangerConsole/>
     </div>
-  );
+  )
 }
 
-export default UserProfile;
+export default UserProfile
